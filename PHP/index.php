aa3b5f83a17674f0ec420d9e0d156b40261a079a
black-night <black_night@gmx.net>
2014-01-30 18:17:22 +0100

    Revision 2

--- PHP/index.php
+++ PHP/index.php
@@ -35,7 +35,7 @@ class PHP extends Plugin {
          $this->lang_admin = new Language($this->PLUGIN_SELF_DIR."sprachen/admin_language_".$ADMIN_CONF->get("language").".txt");
         $info = array(
             // Plugin-Name (wird in der Pluginübersicht im Adminbereich angezeigt)
-             $this->lang_admin->getLanguageValue("plugin_name")." Revision: 1",
+             $this->lang_admin->getLanguageValue("plugin_name")." Revision: 2",
             // CMS-Version
             "2.0",
             // Kurzbeschreibung
